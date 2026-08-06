daec2d33568264289a948384052d41c79aad37ee
SHGT-V2.0 <shglobaltechnology1981@gmail.com>
2026-08-06 13:59:21 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,51 @@
+/*==================================================
+SH STORE
+Login System
+app.js
+==================================================*/
+
+
 function login(){
 
-let user=document.getElementById("username").value;
 
-let pass=document.getElementById("password").value;
+let user =
+document.getElementById("username").value.trim();
+
+
+
+let pass =
+document.getElementById("password").value.trim();
+
+
+
+
 
 if(user==="admin" && pass==="1234"){
 
+
+
+localStorage.setItem(
+"loginUser",
+"admin"
+);
+
+
+
 window.location="dashboard.html";
 
-}else{
 
-document.getElementById("msg").innerHTML="Invalid Username or Password";
 
 }
+
+else{
+
+
+document.getElementById("msg").innerHTML =
+"Invalid Username or Password";
+
+
+}
+
+
 
 }
