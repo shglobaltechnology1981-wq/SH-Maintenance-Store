d99a2c438bca1423868c6a7904e8ab213df46193
SHGT-V2.0 <shglobaltechnology1981@gmail.com>
2026-08-06 16:20:16 +0600

    Update user-auth.js

--- js/user-auth.js
+++ js/user-auth.js
@@ -1,1 +1,99 @@
+//==================================================
+// SH Maintenance Store
+// USER LOGIN
+//==================================================
 
+
+let users = [
+
+
+{
+id:"EMP001",
+name:"Maintenance",
+department:"Maintenance",
+password:"1234"
+},
+
+
+{
+id:"EMP002",
+name:"Electrical",
+department:"Electrical",
+password:"1234"
+},
+
+
+{
+id:"EMP003",
+name:"Production",
+department:"Production",
+password:"1234"
+},
+
+
+{
+id:"EMP004",
+name:"Store",
+department:"Store",
+password:"1234"
+}
+
+
+];
+
+
+// Add More User Here
+// Maximum 20 User
+
+
+function userLogin(){
+
+
+let id =
+document.getElementById("userId").value;
+
+
+let pass =
+document.getElementById("password").value;
+
+
+
+let user = users.find(u=>
+
+u.id==id &&
+u.password==pass
+
+);
+
+
+
+if(user){
+
+
+localStorage.setItem(
+
+"loginUser",
+
+JSON.stringify(user)
+
+);
+
+
+
+window.location.href=
+"requisition.html";
+
+
+}
+
+else{
+
+
+document.getElementById("msg").innerHTML=
+"Invalid User ID or Password";
+
+
+}
+
+
+}
