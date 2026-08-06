e6057c1ab0a1734b4770372e32aacc1d2eac9fba
SHGT-V2.0 <shglobaltechnology1981@gmail.com>
2026-08-06 18:06:22 +0600

    Update login.js

--- js/login.js
+++ js/login.js
@@ -15,28 +15,111 @@ document.getElementById("password").value;
 
 
 
+//==============================
+// ADMIN LOGIN
+//==============================
+
 if(user==="admin" && pass==="1234"){
 
 
+
+let loginUser = {
+
+    id:"ADMIN001",
+
+    name:"Admin",
+
+    department:"Management"
+
+};
+
+
+
 localStorage.setItem(
-"login",
-"true"
+
+"loginUser",
+
+JSON.stringify(loginUser)
+
 );
+
+
+
+localStorage.setItem(
+
+"login",
+
+"true"
+
+);
+
 
 
 window.location="dashboard.html";
 
 
+
 }
+
+
+//==============================
+// EMPLOYEE LOGIN
+//==============================
+
+else if(user==="EMP001" && pass==="1234"){
+
+
+
+let loginUser = {
+
+    id:"EMP001",
+
+    name:"Md. Samsul Haque",
+
+    department:"Maintenance"
+
+};
+
+
+
+localStorage.setItem(
+
+"loginUser",
+
+JSON.stringify(loginUser)
+
+);
+
+
+
+localStorage.setItem(
+
+"login",
+
+"true"
+
+);
+
+
+
+window.location="dashboard.html";
+
+
+
+}
+
+
 
 else{
 
 
-document.getElementById("msg").innerHTML=
+document.getElementById("msg").innerHTML =
+
 "Wrong Username or Password";
 
 
 }
+
 
 
 }
