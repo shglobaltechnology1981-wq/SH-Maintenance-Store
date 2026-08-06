c52f124106d11fbda4d3763c9e96adfe99449797
SHGT-V2.0 <shglobaltechnology1981@gmail.com>
2026-08-06 16:23:03 +0600

    Update user-auth.js

--- js/user-auth.js
+++ js/user-auth.js
@@ -6,41 +6,77 @@
 
 let users = [
 
-
 {
 id:"EMP001",
-name:"Maintenance",
+name:"Md. Samsul Haque",
 department:"Maintenance",
 password:"1234"
 },
 
-
 {
 id:"EMP002",
-name:"Electrical",
+name:"Electrical Team",
 department:"Electrical",
 password:"1234"
 },
 
-
 {
 id:"EMP003",
-name:"Production",
+name:"Mechanical Team",
+department:"Mechanical",
+password:"1234"
+},
+
+{
+id:"EMP004",
+name:"Production Team",
 department:"Production",
 password:"1234"
 },
 
+{
+id:"EMP005",
+name:"Cutting Department",
+department:"Cutting",
+password:"1234"
+},
 
 {
-id:"EMP004",
-name:"Store",
+id:"EMP006",
+name:"Finishing Department",
+department:"Finishing",
+password:"1234"
+},
+
+{
+id:"EMP007",
+name:"Quality Department",
+department:"Quality",
+password:"1234"
+},
+
+{
+id:"EMP008",
+name:"Store Department",
 department:"Store",
+password:"1234"
+},
+
+{
+id:"EMP009",
+name:"Admin Department",
+department:"Admin",
+password:"1234"
+},
+
+{
+id:"EMP010",
+name:"Supervisor",
+department:"Supervisor",
 password:"1234"
 }
 
-
 ];
-
 
 // Add More User Here
 // Maximum 20 User
